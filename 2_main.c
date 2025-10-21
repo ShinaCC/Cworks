8da9c08cd55c5a85287b91f48d8b64e7e2498808
Shina <111693115+ShinaCC@users.noreply.github.com>
2025-10-22 00:49:45 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -4,16 +4,16 @@
 #include <stdio.h>
 int main()
 {
-    int score;
+    int s;
     printf("输入成绩：");
-    scanf("%d",&score);//获取用户输入的成绩
-    if(score >= 90)
+    scanf("%d",&s);
+    if(s >= 90)
         printf("等级为:A");
-    else if(score >= 80)
+    else if(s>= 80)
         printf("等级为:B");
-    else if(score >= 70)
+    else if(s>= 70)
         printf("等级为:C");
-    else if(score >= 60)
+    else if(s >= 60)
         printf("等级为:D");
     else
         printf("等级为:E");
