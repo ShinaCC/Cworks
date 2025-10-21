b2c7fdd2311dfa8444f8161b1b049d266fcc5dc1
Shina <111693115+ShinaCC@users.noreply.github.com>
2025-10-21 23:16:43 +0800

    Convert Celsius to Fahrenheit in main function

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,13 @@
-//
-//
-//
+//202510305216
+//3531783828@qq.com
+//张嘉鑫
 #include <stdio.h>
 int main()
 {
-    printf("Hello World");
+    float a,b;
+    printf("输入摄氏温度");
+    scanf("%f",&a);
+    b = a*9/5 + 32;
+    printf("华氏温度为：%.1f",b);
     return 0;
 }
