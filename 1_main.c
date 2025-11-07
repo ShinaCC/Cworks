031b4abe15c0b55ca930f710d2e0f7f2802b443c
Shina <111693115+ShinaCC@users.noreply.github.com>
2025-11-08 00:18:33 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -12,7 +12,7 @@ int main() {
     }
     for (i = 0; i < 3; i++) {
         for (j = 0; j < 3; j++) {
-            printf("%d ", matrix[i][j]); 
+            printf(" %d", matrix[i][j]); 
         }
         printf("\n"); 
     }
