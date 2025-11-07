93dece69c57dbc08f6c62ed18b665a18d2dc0768
Shina <111693115+ShinaCC@users.noreply.github.com>
2025-11-08 00:37:38 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -2,31 +2,30 @@
 //3531783828@qq.com
 //张嘉鑫
 #include <stdio.h>
-int main()
-{
-    int n;
-    scanf ("%d", &n);
-     if (n <= 1 || n >= 50) {
-        printf("密钥不安全，请重新输入\n");
-    }
-    else{
-    int i = 1;
-    int a = 2;
-    while (a < n)
-    {
-        if(n % a == 0){
-        i = 0;
-        break;
-        }
-        a++;
-    }
 
-    if(i)  {
-        printf("密钥安全，密码设置成功\n");
+int main() {
+    int nums[10];
+    int i, j, temp;
+    for (i = 0; i < 10; i++) {
+        scanf("%d", &nums[i]);
     }
-    else {
-        printf("密钥不安全，请重新输入\n");
+    for (i = 0; i < 9; i++) {
+        for (j = 0; j < 9 - i; j++) {
+            if (nums[j] > nums[j + 1]) {
+                temp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = temp;
+            }
+        }
     }
+    for (i = 0; i < 10; i++) {
+        if (i == 0) {
+            printf("%d", nums[i]);
+        } else {
+            printf(" %d", nums[i]);
+        }
     }
+    printf("\n");
+
     return 0;
 }
