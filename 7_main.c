a2803b9f15594d62a1e7a85ff38fa161cb585244
Shina <111693115+ShinaCC@users.noreply.github.com>
2025-11-08 00:51:36 +0800

    Update 7_main.c

--- 7_main.c
+++ 7_main.c
@@ -2,21 +2,31 @@
 //3531783828@qq.com
 //张嘉鑫
 #include <stdio.h>
+
+void reverseArray(int arr[], int length) {
+    for (int i = 0; i < length / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[length - 1 - i];
+        arr[length - 1 - i] = temp;
+    }
+}
+
 int main() {
     int arr[5];
-    int i;
-    for (i = 0; i < 4; i++) {
+    for (int i = 0; i < 5; i++) {
         scanf("%d", &arr[i]);
     }
-    for (i = 4; i > 0; i--) {
-        arr[i] = arr[i - 1];
-    }
-    arr[0] = 0;
-    for (i = 0; i < 5; i++) {
-        if (i > 0) {
-            printf(" ");
+    
+    reverseArray(arr, 5);
+    
+    for (int i = 0; i < 5; i++) {
+        if (i == 4) {
+            printf("%d", arr[i]);
+        } else {
+            printf("%d ", arr[i]);
         }
-        printf("%d", arr[i]);
     }
+    printf("\n");
+    
     return 0;
 }
