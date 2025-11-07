416f8583b0c9286c5ef906900396c3477e321f55
Shina <111693115+ShinaCC@users.noreply.github.com>
2025-11-08 00:44:32 +0800

    Update 4_main.c

--- 4_main.c
+++ 4_main.c
@@ -2,26 +2,18 @@
 //3531783828@qq.com
 //张嘉鑫
 #include <stdio.h>
-int main()
-{
-    int hundreds, tens, units;
-    int blank = 0;
-    int num = 100;
-    while(num<=999)
-    {
-        hundreds = num / 100;
-        tens = (num / 10) % 10;
-        units = num % 10;
-    if (hundreds * hundreds * hundreds + 
-            tens * tens * tens + 
-            units * units * units == num)   {
-                if(blank > 0){
-                    printf(" ");
-                }
-            
-            printf("%d", num);
-            blank++;
-            }
-        num++;
+int power(int a, int b) {
+    int result = 1;
+    for (int i = 0; i < b; i++) {
+        result *= a;
     }
+    return result;
+}
+int main() {
+    int sum = 0;
+    for (int i = 1; i <= 5; i++) {
+        sum += power(i, 2);
+    }
+    printf("%d\n", sum);
+    return 0;
 }
