5827277a1eb2be997d61da3289e7250bc007658d
Shina <111693115+ShinaCC@users.noreply.github.com>
2025-11-22 01:14:54 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -2,30 +2,17 @@
 //3531783828@qq.com
 //张嘉鑫
 #include <stdio.h>
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
 int main() {
-    int nums[10];
-    int i, j, temp;
-    for (i = 0; i < 10; i++) {
-        scanf("%d", &nums[i]);
-    }
-    for (i = 0; i < 9; i++) {
-        for (j = 0; j < 9 - i; j++) {
-            if (nums[j] > nums[j + 1]) {
-                temp = nums[j];
-                nums[j] = nums[j + 1];
-                nums[j + 1] = temp;
-            }
-        }
-    }
-    for (i = 0; i < 10; i++) {
-        if (i == 0) {
-            printf("%d", nums[i]);
-        } else {
-            printf(" %d", nums[i]);
-        }
-    }
-    printf("\n");
-
+    int x, y;
+    scanf("%d %d", &x, &y);
+    swap(&x, &y);
+    printf("%d %d\n", x, y);
+    
     return 0;
 }
